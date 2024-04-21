d54ff98cd76f9fb308a56b69a80e909c828a3d43
Jannatin Nurrohmah <61218518+Jannatin12@users.noreply.github.com>
2024-04-21 23:16:14 +0700

    Menghapus Comments
    Menghapus Comments yang tidak memiliki nilai karena code sudah menjelaskan bagaimana fungsi setMonthlySalary berjalan

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -31,11 +31,6 @@ public class Employee {
 		children = new LinkedList<>();
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
 	public void setMonthlySalary(int grade) {	
 		int baseSalary = 0;
 		
